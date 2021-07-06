e3be3480d146b27c35a315bb308f4785930beea5
Carl Jidling <carl.jidling@it.uu.se>
2021-07-06 10:22:01 +0200

    full run

--- run_code.py
+++ run_code.py
@@ -1,8 +1,8 @@
 import os
 
-model_name = './model/model2'
+model_name = './model/model3'
 
-os.system('python3 train.py --folder ' +model_name+ ' --epochs 10 '
+os.system('python3 train.py --folder ' +model_name+ ' --epochs 70 '
           '--path_to_data "/scratch/code/date_order.hdf5" --traces_dset "signal" '
           '--examid_dset "id_exam" --path_to_chagas "/scratch/code/chagas.csv" '
-          '--cuda --weight_decay 0.01')
+          '--cuda --weight_decay 0.001')
