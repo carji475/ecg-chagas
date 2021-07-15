67b807312625b4963865d803563a0c7b7d5b246b
Carl Jidling <carl.jidling@it.uu.se>
2021-07-15 16:44:13 +0200

    modified run_code

--- run_code.py
+++ run_code.py
@@ -2,7 +2,17 @@ import os
 
 model_name = './model/model3'
 
+# train
 os.system('python3 train.py --folder ' +model_name+ ' --epochs 70 '
           '--path_to_data "/scratch/code/date_order.hdf5" --traces_dset "signal" '
           '--examid_dset "id_exam" --path_to_chagas "/scratch/code/chagas.csv" '
           '--cuda --weight_decay 0.001')
+
+# evaluate
+os.system('python3 evaluate.py --mdl ' +model_name+
+          '--path_to_traces "/scratch/code/samitrop.hdf5" --traces_dset "signal" '
+          '--examid_dset "id_exam" --path_to_chagas "/scratch/code/chagas.csv" ')
+
+# loss fig
+os.system('python3 plot_learning_curves.py --history_file '+os.path.join(model_name, 'history.csv')+
+          ' --save '+os.path.join(model_name, 'res.pdf'))
