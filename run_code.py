7cc6261d75a422ed1db18b5884479115f7738dff
Carl Jidling <carl.jidling@it.uu.se>
2021-09-07 20:56:03 +0200

    increase weight decay

--- run_code.py
+++ run_code.py
@@ -1,13 +1,13 @@
 import os
 
-model_name = './model/model5'
+model_name = './model/model6'
 
 # train
 os.system('python3 train.py --folder ' + model_name + ' --epochs 70 '
           '--path_to_data "/scratch/code/date_order.hdf5" --traces_dset "signal" '
           '--examid_dset "id_exam" --path_to_chagas "/scratch/code/chagas.csv" '
           '--data_tot 0.3 --dropout_rate 0.4 --valid_split 0.3 --patience 3 '
-          '--ptmdl "./mdl-test" --cuda --weight_decay 0.001')
+          '--ptmdl "./mdl-test" --cuda --weight_decay 0.01')
 
 # evaluate
 os.system('python3 evaluate.py --mdl ' + model_name + ' ' +
