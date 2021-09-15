03ac190385290a3d7baad3c38b83c95cea8d02ba
Carl Jidling <carl.jidling@it.uu.se>
2021-09-15 15:47:41 +0200

    re-added loss weighting

--- run_code.py
+++ run_code.py
@@ -7,7 +7,7 @@ os.system('python3 train.py --folder ' + model_name + ' --epochs 70 '
           '--path_to_data "/scratch/code/date_order.hdf5" --traces_dset "signal" '
           '--examid_dset "id_exam" --path_to_chagas "/scratch/code/chagas.csv" '
           '--data_tot 1.0 --dropout_rate 0.8 --valid_split 0.15 --patience 7 '
-          '--cuda --weight_decay 0.01')
+          '--pos_weight --cuda --weight_decay 0.01')
 
 # evaluate
 os.system('python3 evaluate.py --mdl ' + model_name + ' ' +
